04406afc11c9f95dee5042bc1cd0c87882c4db50
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2023-06-25 11:58:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ api_hash = "f4815b9a16cb03c2f5eabe8db1cb0903"
 
 bot_token = "6293847044:AAEtbqB7bg85cVxauVakjZA6_C-vW2kHSQY"
 
-GROUP_TAG = "RC"
+GROUP_TAG = "AC"
 
-sudo_users = [5261482689,5857774780]
+sudo_users = [5261482689,5857774780,2036297424]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
