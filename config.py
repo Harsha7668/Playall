4553b19072a09cfe7aeb5df16d141f47b2a922db
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2023-07-05 18:48:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ bot_token = "6293847044:AAEtbqB7bg85cVxauVakjZA6_C-vW2kHSQY"
 
 GROUP_TAG = "AC"
 
-sudo_users = [5261482689,5857774780,2036297424]
+sudo_users = [5261482689,5857774780,2036297424,1130243906,5798120731]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
