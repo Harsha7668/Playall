9f6e66b997404c1e58c992f13d4501bcdae3d4ee
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-04-13 14:01:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ api_id = 4857766
 
 api_hash = "6c3c6facf5598a4b318e138f8c407028"
 
-bot_token = "7082142123:AAGZpbB6yYh3kU3BfzalQ4ghmBWhfpx5QTY"
+bot_token = "7082142123:AAHy1dyHjkJGv13uo7n3iAsdHKJhNiNGhIA"
 
 GROUP_TAG = "CARTOONANDANIMEKINGDOM"
 
